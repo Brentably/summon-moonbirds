d3d5ea23cf51bc2c6f4f938080dec38833e3fa49
Brentably <Brent@pabnet.com>
2022-12-02 00:44:29 -0600

    fucked that shit up but I fixed it

--- src/helpers/getContracts.ts
+++ src/helpers/getContracts.ts
@@ -4,7 +4,7 @@ function getContracts(chainID: number): any[] {
 if(chainID == 0) return []
 
 const ManagerABI = SummonManager.abi
-const ManagerAddress = chainID == 1 ? "0xf6F859708839e648B9137f0dc1c70d15029Cc69a" : "0x6f2AB9E7D74c27E53E9050C0f7A57905a9572365"
+const ManagerAddress = chainID == 1 ? "0xdc2E5925598Cde53D37b6b8428aEFc3dc1Ff677C" : "0x6f2AB9E7D74c27E53E9050C0f7A57905a9572365"
 
 const returnArray = new Array<any>(ManagerAddress, ManagerABI)
 // return address and abi
