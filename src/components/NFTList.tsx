f3bbaa56b32b79e2d8b0e9bde9bd2a2fddc8d28b
Brentably <Brent@pabnet.com>
2022-12-07 21:09:14 -0500

    better connect walelt msg

--- src/components/NFTList.tsx
+++ src/components/NFTList.tsx
@@ -79,7 +79,7 @@ function handleLend(asset:IAsset) {
 
 
 if(address == "needs") return <h3 className="sub left">No NFTs Found</h3>
-  if(!address) return <h1>connect wallet</h1>
+  if(!address) return <h1 className='sub'>connect wallet to summon NFTs</h1>
   if(!NFTBalance) return <Loader />
   if(NFTBalance.length < 1 && isSummon) return <h3 className="sub left">No NFTs Found... try lending one to yourself :)</h3>
   if(NFTBalance.length < 1) return <h3 className="sub left">No NFTs Found, <a href="https://goerli-nfts.vercel.app/" target="_blank" rel="noreferrer">mint Goerli NFTs here</a></h3>
